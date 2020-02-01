86180ccf51719715e5fde8780f0d31b54d6f5be3
Teppo Kauppinen <eimink@gmail.com>
2020-02-01 18:50:55 +0200

    updated shader

--- data/shader.glsl
+++ data/shader.glsl
@@ -3,19 +3,78 @@
 uniform float iGlobalTime;
 uniform vec2 iResolution;
 
-uniform float p1;
-uniform float p2;
-uniform float p3;
+uniform vec3 iMouse;
 
+#define NUM_LAYERS 6.
+
+mat2 Rot(float a) {
+    float s=sin(a), c=cos(a);
+    return mat2(c, -s, s, c);
+}
+
+float Star(vec2 uv, float flare) {
+	float d = length(uv);
+    float m = .05/d;
+    
+    float rays = max(0., 1.-abs(uv.x*uv.y*1000.));
+    m += rays*flare;
+    uv *= Rot(3.1415/4.);
+    rays = max(0., 1.-abs(uv.x*uv.y*1000.));
+    m += rays*.3*flare;
+    
+    m *= smoothstep(1., .2, d);
+    return m;
+}
+
+float Hash21(vec2 p) {
+    p = fract(p*vec2(123.34, 456.21));
+    p += dot(p, p+45.32);
+    return fract(p.x*p.y);
+}
+
+vec3 StarLayer(vec2 uv) {
+	vec3 col = vec3(0);
+	
+    vec2 gv = fract(uv)-.5;
+    vec2 id = floor(uv);
+    
+    for(int y=-1;y<=1;y++) {
+    	for(int x=-1;x<=1;x++) {
+            vec2 offs = vec2(x, y);
+            
+    		float n = Hash21(id+offs); // random between 0 and 1
+            float size = fract(n*345.32);
+            
+    		float star = Star(gv-offs-vec2(n, fract(n*34.))+.5, smoothstep(.9, 1., size)*.6);
+            
+            vec3 color = sin(iMouse*fract(n*2345.2)*123.2)*.5+.5;
+            color = color*vec3(1,.25,1.+size)+vec3(.2, .2, .1)*2.;
+            
+            star *= sin(iGlobalTime*3.+n*6.2831)*.5+1.;
+            col += star*size*color;
+        }
+    }
+    return col;
+}
 
 void main(void) {
-        float a = iTime;
-	vec2 uv = (gl_fragCoord.xy-.5*iResolution.xy)/iResolution.y;
-	vec2 st = vec2(atan(uv.x,uv.y)+iGlobalTime,length(uv));
-    uv = vec2(st.x/6.2831,st.y);
+    vec2 uv = (gl_FragCoord-.5*iResolution.xy)/iResolution.y;
+	vec2 M = (iMouse.xy-iResolution.xy*.5)/iResolution.y;
     
-    vec3 p = (0.5*cos(iGlobalTime+sin(12.5*uv.y))+0.5*cos(cos(15.*uv.x)))/2.*vec3(1,uv.y,1);
-    vec3 r = vec3(cos(a)*p.x - sin(a)*p.x,sin(a)*p.y + cos(a)*p.y,p.z);
-    // Output to screen
-    gl_fragColor = vec4(p,1.0);
+    float t = iGlobalTime*.02;
+    
+    uv += M*4.;
+    
+    uv *= Rot(t);
+    vec3 col = vec3(0);
+    
+    for(float i=0.; i<1.; i+=1./NUM_LAYERS) {
+    	float depth = fract(i+t);
+        
+        float scale = mix(20., .5, depth);
+        float fade = depth*smoothstep(1., .9, depth);
+        col += StarLayer(uv*scale+i*453.2-M)*fade;
+    }
+    
+    gl_FragColor = vec4(col,1.0);
 }
