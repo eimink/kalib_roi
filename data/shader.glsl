f8dc953e149cef9198d580cd096cda5cb48339aa
Teppo Kauppinen <eimink@gmail.com>
2020-02-01 19:40:27 +0200

    shader update

--- data/shader.glsl
+++ data/shader.glsl
@@ -5,76 +5,64 @@ uniform vec2 iResolution;
 
 uniform vec3 iMouse;
 
-#define NUM_LAYERS 6.
-
-mat2 Rot(float a) {
-    float s=sin(a), c=cos(a);
-    return mat2(c, -s, s, c);
+float hash21(vec2 co)
+{
+ 	return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
 }
 
-float Star(vec2 uv, float flare) {
-	float d = length(uv);
-    float m = .05/d;
+float star(vec2 uv, vec2 p, float rot, float size)
+{
+    uv-=p;
+    float x = uv.x;
+    float y = uv.y;
+    uv.x = sin(rot)*x + cos(rot)*y;
+    uv.y = cos(rot)*x - sin(rot)*y;
+    uv+=p;
     
-    float rays = max(0., 1.-abs(uv.x*uv.y*1000.));
-    m += rays*flare;
-    uv *= Rot(3.1415/4.);
-    rays = max(0., 1.-abs(uv.x*uv.y*1000.));
-    m += rays*.3*flare;
+    float lm = 1.0-min(abs(uv.x- p.x), abs(uv.y-p.y));
     
-    m *= smoothstep(1., .2, d);
-    return m;
+    float ld = 1.-length(uv - p);
+    ld = max(0., ld);
+    lm = max(0., lm);
+    ld = pow(ld, 2.5);
+    lm = pow(lm, 20.)*ld;
+    
+    return max(0., (lm+ld)*size);
 }
 
-float Hash21(vec2 p) {
-    p = fract(p*vec2(123.34, 456.21));
-    p += dot(p, p+45.32);
-    return fract(p.x*p.y);
-}
-
-vec3 StarLayer(vec2 uv) {
-	vec3 col = vec3(0);
+void mainImage()
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = gl_FragCoord/iResolution.xy;
+    uv.y/= iResolution.x/iResolution.y;
 	
-    vec2 gv = fract(uv)-.5;
-    vec2 id = floor(uv);
-    
-    for(int y=-1;y<=1;y++) {
-    	for(int x=-1;x<=1;x++) {
-            vec2 offs = vec2(x, y);
-            
-    		float n = Hash21(id+offs); // random between 0 and 1
-            float size = fract(n*345.32);
-            
-    		float star = Star(gv-offs-vec2(n, fract(n*34.))+.5, smoothstep(.9, 1., size)*.6);
-            
-            vec3 color = sin(iMouse*fract(n*2345.2)*123.2)*.5+.5;
-            color = color*vec3(1,.25,1.+size)+vec3(.2, .2, .1)*2.;
-            
-            star *= sin(iGlobalTime*3.+n*6.2831)*.5+1.;
-            col += star*size*color;
-        }
-    }
-    return col;
-}
+    uv*=10.;
+    uv.y+=iTime*0.8;
+    vec3 col = vec3(0.);
 
-void main(void) {
-    vec2 uv = (gl_FragCoord-.5*iResolution.xy)/iResolution.y;
-	vec2 M = (iMouse.xy-iResolution.xy*.5)/iResolution.y;
+    vec2 guv = fract(uv);
+    vec2 gx = floor(uv);
     
-    float t = iGlobalTime*.02;
-    
-    uv += M*4.;
-    
-    uv *= Rot(t);
-    vec3 col = vec3(0);
-    
-    for(float i=0.; i<1.; i+=1./NUM_LAYERS) {
-    	float depth = fract(i+t);
+    for( int i = -1; i < 3; i++)
+    {
+     for(int j = -1; j < 3; j++)
+     {
+        vec2 off = vec2(i,j);
+        vec2 gxy =gx+off;
         
-        float scale = mix(20., .5, depth);
-        float fade = depth*smoothstep(1., .9, depth);
-        col += StarLayer(uv*scale+i*453.2-M)*fade;
+        float h = hash21(gxy);
+        
+        vec2 puv = guv-off;
+    	col += star(puv, vec2(h, fract(h*10.))*0.5+0.5, gxy.x+iGlobalTime*0.5, pow(fract(h*100.),3.)) * vec3(h, fract(10.*h),h*.4);
+    	
+     }
     }
+    float h = hash21(gx);
+    //col += star(guv, vec2(h, h), 0.5);
     
+    //if(guv.x < 0.01 || guv.y < 0.01)
+      //  col.r = 1.;
+    
+    // Output to screen
     gl_FragColor = vec4(col,1.0);
 }
